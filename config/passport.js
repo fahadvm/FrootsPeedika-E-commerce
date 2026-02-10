c13fb6732852375899bdab53a205df2b572fa0cb
fahad vm <fahadfad444@gmail.com>
2026-02-10 16:28:36 +0530

    fix:google auth call back url change

--- config/passport.js
+++ config/passport.js
@@ -7,7 +7,8 @@ const env = require("dotenv").config()
 passport.use(new GoogleStrategy({
     clientID:process.env.GOOGLE_CLIENT_ID,
     clientSecret:process.env.GOOGLE_CLIENT_SECRET,
-    callbackURL:"http://localhost:3002/auth/google/callback"
+    // callbackURL:"http://localhost:3002/auth/google/callback" 
+    callbackURL:"https://frootspeedika.shop/auth/google/callback"
 },async(accessToken, refreshToken, profile, done)=>{
     try {
         
